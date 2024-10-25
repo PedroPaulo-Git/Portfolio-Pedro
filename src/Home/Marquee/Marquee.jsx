960700d4869513da7e02111a1b8e5f61920db3e0
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-10-25 10:50:41 -0300

    marquee horizontal using motion

--- src/Home/Marquee/Marquee.jsx
+++ src/Home/Marquee/Marquee.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from "react";
 import { FaNode } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
 import { FaGitAlt } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
 import { SiTypescript } from "react-icons/si";
- const Marquee = () => {
+import { motion } from "framer-motion";
+const Marquee = () => {
   return (
-    <div className='bg-white flex text-4xl h-1/3 -mt-20 z-0 py-auto' >
-        <div className='flex items-center'>
+    <div className="bg-white flex text-6xl h-72 pt-10 -mt-20 z-0 py-auto justify-center overflow-x-hidden">
+      <motion.div
+        initial={{ x: 0 }}
+        animate={{ x: "-100%" }}
+        transition={{ duration: 50 }}
+        className="flex items-center gap-52 "
+      >
         <FaNode />
-<BiLogoPostgresql />
-<RiTailwindCssFill />
-<FaGitAlt />
-<FaGithub />
-<SiTypescript />
-        </div>
-
+        <BiLogoPostgresql />
+        <RiTailwindCssFill />
+        <FaGitAlt />
+        <FaGithub />
+        <SiTypescript />
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 export default Marquee;
