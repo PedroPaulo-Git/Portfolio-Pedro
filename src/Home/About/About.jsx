4b40034db747d650d4611a96063296a5ac015808
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-11-04 13:48:58 -0300

    translations about me

--- src/Home/About/About.jsx
+++ src/Home/About/About.jsx
@@ -1,5 +1,7 @@
 import GridSkils from "./Components/Grid";
+import { useTranslation } from "react-i18next";
 const About = () => {
+  const [t, i18n] = useTranslation("global");
   return (
     <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0 -mt-20 rounded-b-[90px] z-0">
       <div className="absolute inset-0 -z-10 overflow-hidden"></div>
@@ -8,10 +10,10 @@ const About = () => {
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
               <p className="text-base font-semibold leading-7 text-gray-light">
-                Sobre Mim
+                {t("about.about_subtitle")}
               </p>
               <h1 className="mt-2 text-3xl sm:text-2xl lg:text-4xl font-semibold bg-gradient-to-l from-gray-midlight to-black bg-clip-text text-transparent">
-                Percurso Profissional
+                {t("about.about_title")}
               </h1>
               <p className="mt-6 text-xl leading-8 text-gray-700"></p>
             </div>
@@ -21,65 +23,23 @@ const About = () => {
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-              <p>
-                Especialista em desenvolvimento de aplicacoes completas, com
-                forte enfase em interfaces dinamicas utilizando React e
-                habilidades robustas em back<span className="font-sans font-semibold">-</span>end com Node.js e Postgres. Conclui
-                o curso de Analise e Desenvolvimento de Sistemas na UNINASSAU
-               em dezembro de 2023.
-              </p>
+            <p dangerouslySetInnerHTML={{ __html: t('about.about_description') }} />
               <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                <li className="flex gap-x-3">
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      React e Interfaces Interativas.
-                    </strong>{" "}
-                    Construcao de UI<span className="font-sans font-semibold">/</span>UX responsivas focadas em usabilidade e
-                    engajamento.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      Tecnologias de Back<span className="font-sans font-semibold">-</span>end.
-                    </strong>{" "}
-                    Experiencia com Node.js, Express e Postgres para
-                    desenvolvimento de APIs robustas.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      Integracao de APIs.
-                    </strong>{" "}
-                    Utilizacao de Axios para comunicacao eficiente entre
-                    Front<span className="font-sans font-semibold">-</span>end e Back<span className="font-sans font-semibold">-</span>end.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      Design e Landing Pages.
-                    </strong>{" "}
-                    Criacao de layouts que maximizam conversao e experiencia do
-                    usuario.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      Formacao Tecnica.
-                    </strong>{" "}
-                    Curso de Analise e Desenvolvimento de Sistemas, com enfase
-                    em praticas de mercado.
-                  </span>
-                </li>
+              {t('about.about_skills', { returnObjects: true }).map((skill, index) => (
+          <li key={index} className="flex gap-x-3">
+            <span>
+            <strong className="font-semibold text-gray-900" dangerouslySetInnerHTML={{ __html: skill.title }} />
+              <span dangerouslySetInnerHTML={{ __html: skill.description }} />
+            </span>
+          </li>
+        ))}
               </ul>
-              <p className="mt-8">
+              <br></br>
+              {/* <p className="mt-8">
   Com uma forte base em tecnologias Front<span className="font-sans font-semibold">-</span>end e Back<span className="font-sans font-semibold">-</span>end, estou sempre em busca de
   solucoes que unam funcionalidade e estetica. Acredito que a tecnologia deve servir para melhorar a experiencia do usuario e estou aqui para fazer isso acontecer.
-</p>
-
+</p> */}
+            <p dangerouslySetInnerHTML={{ __html: t('about.about_description') }} />
             </div>
           </div>
         </div>
--- src/Home/About/Components/Grid.jsx
+++ src/Home/About/Components/Grid.jsx
@@ -1,84 +1,104 @@
-import React,{useRef,useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import programming from "../../../assets/About/tec.png"; // add other icons as needed
 import IconCodingStudy from "../../../assets/About/coding-book.png"; // add other icons as needed
 import IconBook from "../../../assets/About/book.png";
 import IconSuccess from "../../../assets/About/success.png";
 import { useInView } from "framer-motion";
-import { motion } from "framer-motion"
-
+import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 
 const GridSkils = () => {
-  
-  const ref = useRef(null)
-  const isInView = useInView(ref,{once:true})
+  const [t, i18n] = useTranslation("global");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
-
-useEffect(()=>{
-  console.log(isInView)
-},[isInView]);
-
-
+  useEffect(() => {
+    console.log(isInView);
+  }, [isInView]);
 
   return (
     <motion.div
-    variants = {{
-      hidden:{ opacity: 0, x:30},
-      visible:{ opacity: 1, x:0},
+    variants={{
+      hidden: { opacity: 0, x: 30 },
+      visible: { opacity: 1, x: 0 },
     }}
-  initial="hidden"
-  animate={isInView ? { opacity: 1, x: 0 } : {}}
-  transition={{ duration: 0.5,delay:0.2}}
->
+    initial="hidden"
+    animate={isInView ? { opacity: 1, x: 0 } : {}}
+    transition={{ duration: 0.5, delay: 0.2 }}
+  >
     <div ref={ref} className="lg:w-[30rem] sm:w-[10rem] md:w-[30rem]">
-     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 lg:w-full">
-  <div className="h-32 z-10 flex justify-center items-center bg-white shadow-lg border-b">
-    <img className="w-[4rem] object-contain" src={IconSuccess} alt="Ícone de Troféu" />
-  </div>
- 
-  <div className="h-32 p-2 text-center flex items-center bg-gray-50 shadow-lg border-b">
-      <div className="text-xl w-full">
-        <span className="font-bold">25<span className="font-sans">+</span></span><br></br>
-        <span className="text-base text-gray-light">Projetos Conclu<span className="font-sans">í</span>dos</span>
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 lg:w-full">
+        <div className="h-32 z-10 flex justify-center items-center bg-white shadow-lg border-b">
+          <img
+            className="w-[4rem] object-contain"
+            src={IconSuccess}
+            alt="Ícone de Troféu"
+          />
+        </div>
+
+        <div className="h-32 p-2 text-center flex items-center bg-gray-50 shadow-lg border-b">
+          <div className="text-xl w-full">
+            <span className="font-bold" dangerouslySetInnerHTML={{ __html: t('about_grid.value1') }} />
+            <br />
+            <span className="text-base text-gray-light">{t("about_grid.label1")}</span>
+          </div>
+        </div>
+
+        <div className="h-32 flex justify-center items-center bg-white shadow-lg z-10">
+          <img
+            className="w-[4rem] object-contain"
+            src={programming}
+            alt="Ícone de Certificado"
+          />
+        </div>
+
+        <div className="h-32 p-2 text-center flex items-center bg-gray-50">
+          <div className="text-xl w-full">
+            <span className="font-bold" dangerouslySetInnerHTML={{ __html: t('about_grid.value2') }} />
+            <br />
+            <span className="text-base text-gray-light">{t("about_grid.label2")}</span>
+          </div>
+        </div>
+        
+        <div className="h-32 p-2 text-center flex items-center bg-gray-50">
+          <div className="text-xl w-full">
+            <span className="font-bold" dangerouslySetInnerHTML={{ __html: t('about_grid.value3') }} />
+            <br />
+            <span className="text-base text-gray-light">{t("about_grid.label3")}</span>
+          </div>
+        </div>
+
+        <div className="h-32 flex justify-center items-center bg-white shadow-lg z-10">
+          <img
+            className="w-[4rem] object-contain"
+            src={IconBook}
+            alt="Ícone de Graduação"
+          />
+        </div>
+
+        
+
+        <div className="h-32 p-2 text-center flex items-center bg-gray-50">
+          <div className="text-xl w-full">
+            <span className="font-bold" dangerouslySetInnerHTML={{ __html: t('about_grid.value4') }} />
+            <br />
+            <span className="text-base text-gray-light">{t("about_grid.label4")}</span>
+          </div>
+        </div>
+
+        <div className="h-32 flex justify-center items-center bg-white shadow-lg z-10">
+          <img
+            className="w-[3.2rem] object-contain"
+            src={IconCodingStudy}
+            alt="Ícone de Web"
+          />
+        </div>
+
+       
       </div>
-  </div>
-
-  <div className="h-32 flex justify-center items-center bg-white shadow-lg z-10">
-    <img className="w-[4rem] object-contain" src={programming} alt="Ícone de Certificado" />
-  </div>
-
-  <div className="h-32 p-2 text-center flex items-center bg-gray-50">
-      <div className="text-xl w-full">
-        <span className="font-bold">15<span className="font-sans">+</span></span><br></br>
-        <span className="text-base text-gray-light">Tecnologias Dominadas</span>
-      </div>
-  </div>
-
-  <div className="h-32 p-2 text-center flex items-center bg-gray-50 shadow-lg border-b">
-      <div className="text-xl w-full">
-        <span className="font-bold">21.900<span className="font-sans">+</span></span><br></br>
-        <span className="text-base text-gray-light">Horas de Cursos</span>
-      </div>
-  </div>
-
-  <div className="h-32 flex justify-center items-center bg-white shadow-lg z-10 border-b">
-    <img className="w-[4rem] object-contain" src={IconBook} alt="Ícone de Graduação" />
-  </div>
-  
-  <div className="h-32 p-2 text-center flex items-center bg-gray-50">
-      <div className="text-xl w-full">
-        <span className="font-bold">3.5<span className="font-sans">+</span></span><br></br>
-        <span className="text-base text-gray-light">Anos de Estudos</span>
-      </div>
-  </div>
-
-  <div className="h-32 flex justify-center items-center bg-white shadow-lg z-10">
-    <img className="w-[3.2rem] object-contain" src={IconCodingStudy} alt="Ícone de Web" />
-  </div>
-</div>
-
     </div>
-    </motion.div>
+  </motion.div>
   );
 };
 export default GridSkils;
