82244d3741dab908e67d60969c3cdc393be7bb0b
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-11-04 14:07:30 -0300

    translations projects (work component)

--- src/Home/Work/Work.jsx
+++ src/Home/Work/Work.jsx
@@ -7,102 +7,92 @@ import Template5 from "../../assets/Works/Templete5.png";
 import Template6 from "../../assets/Works/TemplateGame.png";
 import Template7 from "../../assets/Works/TemplateDog.png";
 import { useInView } from "framer-motion";
+import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion"
 
 import { IoLogoGithub } from "react-icons/io5";
 // const [isMobile,setIsMobile] = useState()
+
+
+const Work = () => {
+  
+  const [t, i18n] = useTranslation("global");
+  
 const features = [
   {
     image: IdealTemplate,
-    name: "Plataforma de Restaurante",
+    name: t('work.projects.0.name'), // "Plataforma de Restaurante"
     social: <IoLogoGithub />,
-    where: "Solução Empresarial",
-    date: <span>Novembro 202<span className="font-sans">4</span></span>,
-    href:'https://ideal-restaurante-pedropaulo-git-pedropaulogits-projects.vercel.app/',
-    github:'https://github.com/PedroPaulo-Git/IdealRestaurante',
-    description:
-      "Desenvolvimento de plataforma completa para restaurantes, com integração Stripe para pagamentos, dashboard administrativo e tecnologias modernas como React, Axios, Prisma e PostgreSQL para garantir alta performance e segurança.",
+    where: t('work.projects.0.where'), // "Solução Empresarial"
+    date: <span>{t('work.projects.0.date')}</span>, // "Novembro 2023"
+    href: 'https://ideal-restaurante-pedropaulo-git-pedropaulogits-projects.vercel.app/',
+    github: 'https://github.com/PedroPaulo-Git/IdealRestaurante',
+    description: t('work.projects.0.description'), // Descrição do projeto
   },
   {
     image: Template2,
-    name: "Landing Page Profissional",
+    name: t('work.projects.1.name'), // "Landing Page Profissional"
     social: <IoLogoGithub />,
-    where: "Desenvolvimento Profissional",
-    date: <span>Agosto 202<span className="font-sans">4</span></span>,
-    href:'https://pedropaulo-git.github.io/Geison-Fisioterapeuta/',
-    github:'https://github.com/PedroPaulo-Git/Geison-Fisioterapeuta',
-    description:
-      "Landing page moderna e responsiva, criada com React e Tailwind para otimizar a experiência do usuário e garantir desempenho elevado. Focada em conversão e segurança, inclui integração com certificados SSL para proteger dados e fortalecer a presença digital da marca.",
+    where: t('work.projects.1.where'), // "Desenvolvimento Profissional"
+    date: <span>{t('work.projects.1.date')}</span>, // "Agosto 2023"
+    href: 'https://pedropaulo-git.github.io/Geison-Fisioterapeuta/',
+    github: 'https://github.com/PedroPaulo-Git/Geison-Fisioterapeuta',
+    description: t('work.projects.1.description'), // Descrição do projeto
   },
   {
     image: Template3,
-    name: "Portfolio Profissional",
+    name: t('work.projects.2.name'), // "Portfolio Profissional"
     social: <IoLogoGithub />,
-    where: "Projeto de Software House",
-    date:<span>Junho 202<span className="font-sans">4</span></span>,
-    href:'https://pedropaulo-git.github.io/PortSoftware/',
-    github:'https://github.com/PedroPaulo-Git/PortSoftware',
-
-    description:
-      "Portfólio pessoal desenvolvido com React, Vite e TailwindCSS, criado como parte da fundação da minha software house. Focado em design intuitivo e funcionalidade responsiva, o projeto demonstra expertise em soluções web e fortalece a identidade digital da marca.",
+    where: t('work.projects.2.where'), // "Projeto de Software House"
+    date: <span>{t('work.projects.2.date')}</span>, // "Junho 2023"
+    href: 'https://pedropaulo-git.github.io/PortSoftware/',
+    github: 'https://github.com/PedroPaulo-Git/PortSoftware',
+    description: t('work.projects.2.description'), // Descrição do projeto
   },
   {
     image: Template5,
-    name: "Sistema de Gerenciamento de Pets",
+    name: t('work.projects.3.name'), // "Sistema de Gerenciamento de Pets"
     social: <IoLogoGithub />,
-    where: "Processo Seletivo",
-    date:<span>Dezembro 202<span className="font-sans">3</span></span>,
-    href:'https://github.com/PedroPaulo-Git/desafio-junior-1',
-    github:'https://github.com/PedroPaulo-Git/desafio-junior-1',
-    description:
-      "Desenvolvimento de um sistema completo de gerenciamento de pets com funcionalidades CRUD, utilizando Next.js. O projeto foi criado para um processo seletivo, demonstrando experiência em desenvolvimento backend e frontend com foco em performance e escalabilidade.",
+    where: t('work.projects.3.where'), // "Processo Seletivo"
+    date: <span>{t('work.projects.3.date')}</span>, // "Dezembro 2023"
+    href: 'https://github.com/PedroPaulo-Git/desafio-junior-1',
+    github: 'https://github.com/PedroPaulo-Git/desafio-junior-1',
+    description: t('work.projects.3.description'), // Descrição do projeto
   },
   {
     image: Template4,
-    name: "Aplicacao de Filmes",
+    name: t('work.projects.4.name'), // "Aplicacao de Filmes"
     social: <IoLogoGithub />,
-    where: "Projeto Pessoal",
-    date: <span>Dezembro 2023</span>,
-    href:'https://pedropaulo-git.github.io/MoviesRating/',
-    github:'https://github.com/PedroPaulo-Git/MoviesRating',
-    description:
-      "Desenvolvimento de uma aplicação que realiza chamadas GET em uma API externa de filmes utilizando Axios. A aplicação exibe informações detalhadas sobre os filmes, incluindo avaliações por estrelas e dados relevantes, proporcionando uma experiência interativa e informativa ao usuário.",
-    
+    where: t('work.projects.4.where'), // "Projeto Pessoal"
+    date: <span>{t('work.projects.4.date')}</span>, // "Dezembro 2023"
+    href: 'https://pedropaulo-git.github.io/MoviesRating/',
+    github: 'https://github.com/PedroPaulo-Git/MoviesRating',
+    description: t('work.projects.4.description'), // Descrição do projeto
   },
   {
     image: Template6,
-    name: "Jogo Pixelado",
+    name: t('work.projects.5.name'), // "Jogo Pixelado"
     social: <IoLogoGithub />,
-    where: "Projeto Pessoal",
-    date: <span>Dezembro 2023</span>,
+    where: t('work.projects.5.where'), // "Projeto Pessoal"
+    date: <span>{t('work.projects.5.date')}</span>, // "Dezembro 2023"
     href: 'https://github.com/PedroPaulo-Git/CoinClick',
     github: 'https://github.com/PedroPaulo-Git/CoinClick',
-    description:
-    "Prototipo de um jogo pixelado em JavaScript, criado para demonstrar a logica de batalha. O jogo apresenta combates dinâmicos em um mundo retro, ilustrando conceitos fundamentais de programação e design de jogos.",
+    description: t('work.projects.5.description'), // Descrição do projeto
     mobileOnly: true,
-}
-,
-{
-  image: Template7,
-name: "Dog Breed Matcher",
-social: <IoLogoGithub />,
-where: "Projeto Pessoal",
-date: <span>Dezembro 2023</span>,
-href: 'https://github.com/PedroPaulo-Git/What-Breed-Is-My-Dog',
-github: 'https://github.com/PedroPaulo-Git/What-Breed-Is-My-Dog',
-description:
-  "Aplicação que busca imagens de raças de cães de uma API. Cada imagem permite comparar a aparência da raça com a do seu próprio cachorro, ajudando na identificação visual e explorando a diversidade de raças. Desenvolvida com React e Axios.",
-// isMobile: true,
-
-  
-}
-
+  },
+  {
+    image: Template7,
+    name: t('work.projects.6.name'), // "Dog Breed Matcher"
+    social: <IoLogoGithub />,
+    where: t('work.projects.6.where'), // "Projeto Pessoal"
+    date: <span>{t('work.projects.6.date')}</span>, // "Dezembro 2023"
+    href: 'https://github.com/PedroPaulo-Git/What-Breed-Is-My-Dog',
+    github: 'https://github.com/PedroPaulo-Git/What-Breed-Is-My-Dog',
+    description: t('work.projects.6.description'), // Descrição do projeto
+  },
 ];
-
-const Work = () => {
   const ref = useRef(null)
   const isInView = useInView(ref,{once:true})
-
 
 useEffect(()=>{
   console.log(isInView)
@@ -114,13 +104,14 @@ useEffect(()=>{
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center text-center">
           <p className="mt-2 text-2xl sm:text-2xl lg:text-4xl font-semibold bg-gradient-to-l from-gray-midlight to-black bg-clip-text text-transparent">
-            Trabalhos Selecionados
+           {t('work.work_title')}
           </p>
-          <p className="mt-6 text-lg leading-8 text-gray-main">
+          {/* <p className="mt-6 text-lg leading-8 text-gray-main">
             Uma coleção dos projetos mais impactantes, escolhidos para
             representar a qualidade e dedicação em cada detalhe. Explore alguns
             dos meus melhores resultados.
-          </p>
+          </p> */}
+          <p className="mt-6 text-lg leading-8 text-gray-main">{t('work.work_subtitle')}</p> 
         </div>
         <div ref={ref}  className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
